29a2961992b7b63dfd6a4252790e334b76253557
Mickael <brenoit.mickael@gmail.com>
2018-08-06 15:56:13 +0200

    List of posts is now displayed.

---
+++ app/src/main/java/com/mickaelbrenoit/demo/adapter/ListPostsAdapter.java
@@ -0,0 +1,81 @@
+package com.mickaelbrenoit.demo.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mickaelbrenoit.demo.R;
+import com.mickaelbrenoit.demo.api.model.PostApi;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ListPostsAdapter extends RecyclerView.Adapter<ListPostsAdapter.ViewHolderPost> {
+
+    private static final String TAG = "ListPostsAdapter";
+
+    private List<PostApi> postApiList;
+
+    public ListPostsAdapter(List<PostApi> postApiList) {
+        this.postApiList = postApiList;
+    }
+
+    public class ViewHolderPost extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.textView_title_post)
+        TextView textView_title_post;
+        @BindView(R.id.textView_content_post)
+        TextView textView_content_post;
+        @BindView(R.id.textView_display_content_post)
+        TextView textView_display_content_post;
+
+        @BindView(R.id.imageView_arrow_up_post)
+        ImageView imageView_arrow_up_post;
+
+
+        public ViewHolderPost(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderPost onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_post, parent, false);
+        return new ViewHolderPost(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolderPost holder, int position) {
+        final PostApi postApi = postApiList.get(position);
+
+        holder.textView_title_post.setText(postApi.getTitle());
+        holder.textView_content_post.setText(postApi.getBody());
+
+        holder.imageView_arrow_up_post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (holder.textView_content_post.getVisibility() == View.VISIBLE) {
+                    holder.imageView_arrow_up_post.animate().rotation(0).start();
+                    holder.textView_content_post.setVisibility(View.GONE);
+                }
+                else {
+                    holder.imageView_arrow_up_post.animate().rotation(180).start();
+                    holder.textView_content_post.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return postApiList.size();
+    }
+}
--- app/src/main/java/com/mickaelbrenoit/demo/fragment/PostFragment.java
+++ app/src/main/java/com/mickaelbrenoit/demo/fragment/PostFragment.java
@@ -4,17 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mickaelbrenoit.demo.R;
+import com.mickaelbrenoit.demo.adapter.ListPostsAdapter;
 import com.mickaelbrenoit.demo.api.JsonApi;
 import com.mickaelbrenoit.demo.api.model.PostApi;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -27,12 +33,15 @@ public class PostFragment extends Fragment {
 
     private static final String TAG = "PostFragment";
 
+    @BindView(R.id.recyclerView_posts)
+    RecyclerView recyclerView_posts;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_post, null);
 
-        Log.d(TAG, "onCreateView: in");
+        ButterKnife.bind(this, view);
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
 
@@ -50,6 +59,14 @@ public class PostFragment extends Fragment {
                     // TODO - insert all posts in database
                     Log.d(TAG, "onResponse: --> " + postApi.getTitle());
                 }
+
+                // instantiate recyclerView
+                ListPostsAdapter listPostsAdapter = new ListPostsAdapter(response.body());
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+                recyclerView_posts.setLayoutManager(mLayoutManager);
+                recyclerView_posts.setItemAnimator(new DefaultItemAnimator());
+//        recyclerView_liste_interventions.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+                recyclerView_posts.setAdapter(listPostsAdapter);
             }
 
             @Override
